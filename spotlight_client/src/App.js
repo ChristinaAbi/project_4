57a8938dc7b40a721c574d528b41ce8d45b09ff9
ChristinaAbi <clc731e.com>
2020-09-24 17:06:48 -0400

    Updated README, added some styling- Form is not clearing nor posting

--- spotlight_client/src/App.js
+++ spotlight_client/src/App.js
@@ -12,82 +12,87 @@ export default function App(props) {
   })
   const getArtists = async () => {
     try {
-        const response = await fetch('http://localhost:3000/artists');
-        const data = await response.json();
-        console.log(data)
-        setArtists(data);
+      const response = await fetch('http://localhost:3000/artists');
+      const data = await response.json();
+      console.log(data)
+      setArtists(data);
     } catch (error) {
-        console.error(error)
+      console.error(error)
     }
-}
-useEffect(() => {
+  }
+  useEffect(() => {
     (
-        async function (){
-            await getArtists()
-        }
+      async function () {
+        await getArtists()
+      }
     )()
-}, []);
+  }, []);
 
   const handleChange = (event) => {
     const updateInput = Object.assign({}, formInputs, { [event.target.id]: event.target.value })
     updateFormInputs(updateInput)
   }
 
-  const handleSubmit  = async (event) =>{
+  const handleSubmit = async (event) => {
     event.preventDefault()
-    try{
+    try {
       const response = await axios.post('http://localhost:3000/artists', formInputs);
-      const data = response.data;
+      const createdArtist = response.data;
       updateFormInputs({
-            name: "",
-            biography: "",
-            age: "",
-            location: ""
+        name: "",
+        biography: "",
+        age: "",
+        location: ""
       })
-      setArtists([data, ...artists])
-    }catch(error){
+      setArtists([createdArtist, ...artists])
+    } catch (error) {
       console.error(error)
     }
   }
   return (
     <div className="App">
       <div className="container">
-        <h1>
+        <h1 className="header">
           Spotlight Artists
         </h1>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="artist">Artist Name: </label>
-            <input 
-              type="text" 
-              id="name" 
-              value={formInputs.name}
-              onChange={handleChange}/>
-            <label htmlFor="biography">Biography: </label>
-            <input 
-              type="text-area" 
-              id="biography" 
-              value={formInputs.biography}
-              onChange={handleChange}/>
-            <label htmlFor="age">Age: </label>
-            <input 
-              type="text" 
-              id="age" 
-              value={formInputs.age}
-              onChange={handleChange}/>
-            <label htmlFor="location">Location: </label>
-            <input 
-              type="text" 
-              id="location" 
-              value={formInputs.location}
-              onChange={handleChange}/>
-            <input type="submit" className="submit" />
-        </form> 
+        <Artists artists={artists} />
         <br />
-        <main>
-          <h3>
+        <h3>
           Looking to Join Our Spotlight Artists? Fill the form below to be featured!
-          </h3>
-          <Artists artists={artists} />
+        </h3>
+        <main>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Artist Name: </label>
+            <input
+              type="text"
+              id="name"
+              value={formInputs.name}
+              onChange={handleChange} />
+            <br />
+            <label htmlFor="biography">Biography: </label>
+            <input
+              type="text-area"
+              id="biography"
+              value={formInputs.biography}
+              onChange={handleChange} />
+            <br />  
+            <label htmlFor="age">Age: </label>
+            <input
+              type="text"
+              id="age"
+              value={formInputs.age}
+              onChange={handleChange} />
+            <br />  
+            <label htmlFor="location">Location: </label>
+            <input
+              type="text"
+              id="location"
+              value={formInputs.location}
+              onChange={handleChange} />
+            <br />  
+            <input type="submit" className="submit" />
+          </form>
+          <br />
         </main>
       </div>
     </div>
