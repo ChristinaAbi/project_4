91b3b6764a516a65a67cc19bdbf72dc8309d8c84
ChristinaAbi <clc731e.com>
2020-09-24 13:15:08 -0400

    Got map working, displaying all correct info by fixing promise error- added await to response.json to fix problem

--- spotlight_client/src/components/Artists.js
+++ spotlight_client/src/components/Artists.js
@@ -1,9 +1,44 @@
 import React , {useState, useEffect } from 'react';
 
 export default function Artists (props) {
+    const [artists, setArtists] = useState([])
+    const getArtists = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/artists');
+            const data = await response.json();
+            console.log(data)
+            setArtists(data);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    useEffect(() => {
+        (
+            async function (){
+                await getArtists()
+            }
+        )()
+    }, []);
     return (
-        <h1>
-            Upcoming Artists
-        </h1>
+         <div>
+            {artists.map( artist => {
+                return (
+                    <div key={artist.id} className="artist">
+                        <h2>
+                            {artist.name}
+                        </h2>
+                        <p>
+                            Biography: {artist.biography}
+                        </p>
+                        <p>
+                            Age: {artist.age}
+                        </p>
+                        <p>
+                            Location: {artist.location}
+                        </p>
+                    </div>
+                )
+            })}
+         </div>
     )
 }
